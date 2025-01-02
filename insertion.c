9ef6d92eb87b211eb14c03d5f20f02019cc3aef5
Rohit Singh <rs21Rohit@gmail.com>
2025-01-02 13:43:48 +0530

    insertion display property

--- insertion.c
+++ insertion.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
+void display(int arr[] , int n ){
+	for(int i = 0; i<n;i++){
+		printf("%d\n",arr[i]);
+	}
+}
+
 int main(){
 	int arr[100]= {1,3,4,555};
+	display(arr, 4);
 
 }
