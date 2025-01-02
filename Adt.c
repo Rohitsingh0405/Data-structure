f162d4ec2ad780f8b0d3f19544759178ddb1224f
Rohit Singh <rs21Rohit@gmail.com>
2025-01-02 12:59:44 +0530

    4

--- Adt.c
+++ Adt.c
@@ -13,7 +13,15 @@ void init(struct new *b,int t, int s ){
 	b->uses_size= s;
 	b->p=(int*)malloc(t*sizeof(int));
 }
+void input(struct new *b){
+	int n; 
+	for(int i =0 ; i< b->uses_size ;i++){
+		printf("Enter the value %d", i);
+		scanf("%d",&n);
+		(b->p)[i] = n;
 
+	}
+}
 int main(){
 	struct new a;
 	return 0;
