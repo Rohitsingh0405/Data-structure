3c837dd3dd9e9bdda2ce4dc4500de46dbf164cdd
Rohit Singh <rs21Rohit@gmail.com>
2025-01-02 13:56:57 +0530

    insertion code

--- insertion.c
+++ insertion.c
@@ -7,9 +7,26 @@ void display(int arr[] , int n ){
 	printf("\n");
 }
 
+int indexxInsertion(int arr[], int size, int element, int capacity, int index){
+	if(size>=capacity){
+		return -1;
+	}
+	for(int i = size-1 ; i>=index; i--){
+		arr[i+1] = arr[i];
+	}
+	arr[index] = element;
+	return 1;
+}
 
 int main(){
-	int arr[100]= {1,3,4,555};
-	display(arr, 4);
+	int arr[100]= {7,8,12,27,88};
+	int size = 5; 
+	display(arr, size);
+	int element = 45;
+	int capacity = 100;
+	int index = 3;
+	indexxInsertion(arr, size, element, capacity,index);
+	size+=1;
+	display(arr,size);
 
 }
