fadc582fd63a14d56df148861b5c8aa92a13a843
Rohit Singh <rs21Rohit@gmail.com>
2025-01-02 12:22:40 +0530

    1

--- ArrayADT.c
+++ ArrayADT.c
@@ -1,43 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-struct myArray
-{
-	int total_size;
-	int used_size;
-	int *ptr ;
 
+struct myStruct{
+	int total_size;
+	int used_size ;
+	int *ptr;
 };
 
-void createArray(struct myArray * a, int tSize , int uSize){
-	// (*a).total_size =tSize; 
-	// (*a).used_size =uSize; 
-	// (*a).ptr = (int *) malloc(tSize*sizeof(int));
-
-	a->total_size= tSize;
-	a->used_size= uSize;
-	a->ptr = (int *)malloc(tSize * sizeof(int));
+void value(struct myStruct *a , int t , int s){
+	a->total_size = t;
+	a->used_size = s;
+	a->ptr = (int *)malloc(t*sizeof(int));
 }
 
-void enter(struct myArray *a){
-		int n ;
-	for(int i = 0; i< a->used_size ;i++){
-		printf("Enter the element %d", i);
-		scanf("%d", &n);
-		(a->ptr)[i] = n;
-	}
-	
-}
-void show(struct myArray *a){
-	for(int i = 0; i< a->used_size;i++){
-
-		printf("%d\n", (a->ptr)[i]);
-	}
-}
 int main(){
-	struct myArray marks;
-	createArray(&marks, 100,2);
-	enter(&marks);
-	show(&marks);
+	struct myStruct arr ;
+	value(&arr , 10, 2);
+
 	return 0;
-	
 }
