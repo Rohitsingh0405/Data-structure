2566e1b30b7f2522b7d75722f7245d90a0151fba
Rohit Singh <rs21Rohit@gmail.com>
2025-01-03 09:22:35 +0530

    when index is 0

---
+++ Deletion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void delete(int arr[],int ind){
+for(int i = ind; i<5;i++){
+	arr[i] = arr[i+1];
+}
+
+}
+void print(int arr[]){
+	for(int i = 0 ;i<4;i++){
+		printf("%d ",arr[i]);
+	}
+}
+int main(){
+	int arr[] = {10,45,32,54,65};
+	int index = 0;
+	delete(arr,index);
+	print(arr);
+	return 0;
+}
