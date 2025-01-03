133454fa358105e9c9036207e58efab815924b7e
Rohit Singh <rs21Rohit@gmail.com>
2025-01-03 10:33:02 +0530

    one more change now i am getting the index instead of the  number

--- Binary_Search.c
+++ Binary_Search.c
@@ -21,12 +21,12 @@ int BinarySearch(int arr[], int element, int size)
 	int mid, high,low;
 
 	low = 0;
-	high = arr[size-1];
+	high =size-1;
 	while(low<=high){
 
 	mid = (low + high)/2;
 	if(arr[mid] == element){
-		return element;
+		return mid;
 	}
 	if(arr[mid]<element){
 		low = mid+1;
@@ -41,15 +41,16 @@ int BinarySearch(int arr[], int element, int size)
 int main()
 {
 	int arr[] = {12, 54, 65, 32, 12};
-	int e = 65;
+	int e = 12;
 	// int size = 5;
 	int size = sizeof(arr) / sizeof(int);
-	// int start = 0;
+
 	// int a = linerSearch(arr,e,size);
-	// printf("%d",a);
+	
 	
 	int a = BinarySearch(arr, e, size);
-	printf("%d ", a);
+
+	printf("The element was found at index %d ", a);
 
 	
 }
